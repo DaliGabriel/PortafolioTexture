eee2e10591f02ad07c9f81d8eedf05530cb4456b
Gabriel Dalí <94218227+DaliGabriel@users.noreply.github.com>
2025-02-08 14:25:43 -0600

    Update ContactMeButton.tsx

--- src/app/components/Hero/ContactMeButton.tsx
+++ src/app/components/Hero/ContactMeButton.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const ContactMeButton = () => {
   const cvUrl =
-    "https://raw.githubusercontent.com/DaliGabriel/PortafolioTexture/main/Cv/CvDaliRangel.pdf"; // Direct link to the CV
+    "https://raw.githubusercontent.com/DaliGabriel/PortafolioTexture/main/Cv/CvDaliRangel.pdf";
 
   return (
     <a
       href={cvUrl}
       target="_blank"
       rel="noopener noreferrer"
-      className="bg-blue-800 hover:bg-blue-950 text-white font-bold py-2 px-4 border border-blue-950 rounded mt-5 inline-block text-center"
+      className="relative z-50 pointer-events-auto bg-blue-800 hover:bg-blue-950 text-white font-bold py-2 px-4 border border-blue-950 rounded mt-5 inline-block text-center"
     >
       Download CV
     </a>
