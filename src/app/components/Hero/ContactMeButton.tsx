cb7e3b8842333906169fadf1da07be1dbe6b4d7e
Gabriel Dalí <94218227+DaliGabriel@users.noreply.github.com>
2025-02-08 14:00:10 -0600

    Update ContactMeButton.tsx

--- src/app/components/Hero/ContactMeButton.tsx
+++ src/app/components/Hero/ContactMeButton.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 const ContactMeButton = () => {
+  const handleDownload = () => {
+    const cvUrl =
+      "https://raw.githubusercontent.com/DaliGabriel/PortafolioTexture/main/Cv/CvDaliRangel.pdf"; // Direct link to the CV
+
+    const link = document.createElement("a");
+    link.href = cvUrl;
+    link.download = "CvDaliRangel.pdf"; // Suggested file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link); // Clean up
+  };
+
   return (
-    <button className="bg-blue-800 hover:bg-blue-950 text-white font-bold py-2 px-4 border border-blue-950 rounded mt-5">
-      Button
+    <button
+      onClick={handleDownload}
+      className="bg-blue-800 hover:bg-blue-950 text-white font-bold py-2 px-4 border border-blue-950 rounded mt-5"
+    >
+      Download CV
     </button>
   );
 };
