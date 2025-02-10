27fcecc8e516419c8816466de28435a171576625
Gabriel Ahumada <dali.rangel98@gmail.com>
2025-02-10 16:04:13 -0600

    updated links

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -5,13 +5,17 @@ import { PiGithubLogoLight, PiLinkedinLogoLight } from "react-icons/pi";
 const Footer = () => {
   return (
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center py-3 bg-slate-50 dark:bg-slate-950">
-      <Link href={"/interactive-map"}>
+      <Link href={"https://github.com/DaliGabriel"}>
         <PiGithubLogoLight
           className="text-black dark:text-white text-xl"
           title="Explore Interactive Map"
         />
       </Link>
-      <Link href={"/interactive-map"}>
+      <Link
+        href={
+          "https://www.linkedin.com/in/dal%C3%AD-rangel-x?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+        }
+      >
         <PiLinkedinLogoLight
           className="text-black dark:text-white text-2xl"
           title="Explore Interactive Map"
