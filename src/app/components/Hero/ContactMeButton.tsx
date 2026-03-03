2e300f58a6bd7c4fb0b3a06f04c26e6f38057980
Gabriel Dali <dali.rangel98@gmail.com>
2026-03-02 21:42:15 -0600

    CV fix

--- src/app/components/Hero/ContactMeButton.tsx
+++ src/app/components/Hero/ContactMeButton.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 
 const ContactMeButton = () => {
-  const cvUrl =
-    "https://raw.githubusercontent.com/DaliGabriel/PortafolioTexture/main/Cv/CvDaliRangel.pdf";
-
   return (
     <a
-      href={cvUrl}
-      target="_blank"
-      rel="noopener noreferrer"
+      href="/CvDaliRangel.pdf"
+      download="CvDaliRangel.pdf"
       className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg transition duration-300"
     >
       Download CV
